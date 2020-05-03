7c163f03c80d81eeed9bfb4b7cfd72a80e4da7fb
xxwikkixx <wikkii@msn.com>
2020-05-03 17:57:29 -0400

    images

--- app/src/main/java/com/waqas/wallstreetbets/SettingsActivity.java
+++ app/src/main/java/com/waqas/wallstreetbets/SettingsActivity.java
@@ -1,12 +1,16 @@
 package com.waqas.wallstreetbets;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreference;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -31,6 +35,22 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
 
+            SwitchPreference switchLB = getPreferenceManager().findPreference("themeColor");
+            switchLB.setSummaryOn("Dark Mode");
+            switchLB.setSummaryOff("Light Mode");
+            switchLB.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    SharedPreferences sharedpref = getContext().getSharedPreferences("sharedPref", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedpref.edit();
+                    editor.putBoolean("Switch", true);
+                    Toast.makeText(getContext(), "This is one now", Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+            });
+
+
+
             Preference logout = getPreferenceManager().findPreference("logout");
             logout.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
                 @Override
@@ -44,5 +64,7 @@ public class SettingsActivity extends AppCompatActivity {
         }
     }
 
+    public void saveData(){
 
+    }
 }
