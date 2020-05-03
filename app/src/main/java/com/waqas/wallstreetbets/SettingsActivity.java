fce329ebe7e19486525641cfcfac4abae9202592
xxwikkixx <wikkii@msn.com>
2020-05-03 01:11:03 -0400

    logout button settings activity

--- app/src/main/java/com/waqas/wallstreetbets/SettingsActivity.java
+++ app/src/main/java/com/waqas/wallstreetbets/SettingsActivity.java
@@ -1,9 +1,11 @@
 package com.waqas.wallstreetbets;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -29,7 +31,18 @@ public class SettingsActivity extends AppCompatActivity {
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
 
-
+            Preference logout = getPreferenceManager().findPreference("logout");
+            logout.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                    startActivity(intent);
+                    getActivity().finishAffinity();
+                    return true;
+                }
+            });
         }
     }
+
+
 }
